70161fe42466a1c98198672b42f56cc2d5765a74
nayvok <44442827+nayvok@users.noreply.github.com>
2025-03-01 15:05:46 +0500

    fix: obj.hasOwnProperty is not a function

--- src/shared/interceptors/big-int-serialize.interceptor.ts
+++ src/shared/interceptors/big-int-serialize.interceptor.ts
@@ -13,18 +13,23 @@ export class BigIntSerializerInterceptor implements NestInterceptor {
         return next.handle().pipe(map(data => this.serializeBigInt(data)));
     }
 
-    private serializeBigInt(obj: any): any {
+    private serializeBigInt(obj: any, seen = new WeakSet()): any {
         if (typeof obj === 'bigint') {
             return obj.toString();
         }
         if (Array.isArray(obj)) {
-            return obj.map(item => this.serializeBigInt(item));
+            return obj.map(item => this.serializeBigInt(item, seen));
         }
         if (typeof obj === 'object' && obj !== null) {
+            if (seen.has(obj)) {
+                return;
+            }
+            seen.add(obj);
+
             const result: any = {};
             for (const key in obj) {
-                if (obj.hasOwnProperty(key)) {
-                    result[key] = this.serializeBigInt(obj[key]);
+                if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                    result[key] = this.serializeBigInt(obj[key], seen);
                 }
             }
             return Object.keys(result).length !== 0 ? result : obj;
